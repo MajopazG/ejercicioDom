9ba86f579c848bbe91ee28c89ba1721d4cd58cee
Maria Jose Paz <majopazgarcia25@gmail.com>
2022-11-08 20:33:06 -0500

    first part of the exercise carried out

--- practicaDom.js
+++ practicaDom.js
@@ -1,11 +1,10 @@
 const title = document.getElementById("title");
-const name = document.getElementById("inputName");
+const userName = document.getElementById("inputuserName");
 const note_one = document.getElementById("inputNote_one");
 const note_two = document.getElementById("inputNote_two");
 const note_three = document.getElementById("inputNote_three");
 const average = document.getElementById("average");
 const textTemporary = document.getElementById("textTemporary");
-const description1 = document.getElementById("description1");
 const description2 = document.getElementById("description2");
 
 window.addEventListener("DOMContentLoaded", mainView);
@@ -18,38 +17,32 @@ function mainView() {
   title.style.textShadow = "-2px -2px 1px #000, 2px 2px 1px #000, -2px 2px 1px #000, 2px -2px 1px #000";
 }
 
-note_one.addEventListener("keyup", calculate);
-note_two.addEventListener("keyup", calculate);
+
 note_three.addEventListener("keyup", calculate);
 
 function calculate(event) {
   if(event.code === "Enter"){
+    if(note_one.value >=0 && note_one.value <= 5.0 && note_two.value >=0 && note_two.value <= 5.0 && note_three.value >=0 && note_three.value <= 5.0 ){
     textTemporary.textContent = "";
-     definitive = ((parseFloat(note_one.value))*0.3 + (parseFloat(note_two.value))*0.3 + (parseFloat(note_three.value))*0.4);
-     finalRounding = definitive.toFixed(1);
-     average.textContent = "El promedio de las notas, del estudiante " + name.value + " es de "+finalRounding;
-      if(finalRounding >= 3.5){
-        description1.textContent = "Usted gano la materia";  /* > es mayor */
-        } else{
-        description1.textContent = "Usted no gano la materia"; 
-        }
+    definitive = ((parseFloat(note_one.value))*0.3 + (parseFloat(note_two.value))*0.3 + (parseFloat(note_three.value))*0.4);
+    finalRounding = definitive.toFixed(2);
+    average.textContent = "El promedio de las notas, del estudiante " + userName.value + " es de "+finalRounding;
+    }
   }
 }
 
-note_one.addEventListener("keyup", casesOfTheNotas);
-note_two.addEventListener("keyup", casesOfTheNotas);
 note_three.addEventListener("keyup", casesOfTheNotas);
 
 function casesOfTheNotas(event){
   if(event.code === "Enter"){
     if (finalRounding >= 4.6){
-      description2.textContent = "El estudiante " + name.value +" es considerado Excelente";
+      description2.textContent = "El estudiante " + userName.value +" es considerado Excelente y gano la materia";
     }else if (finalRounding >= 3.5){
-      description2.textContent = "El estudiante " + name.value +" es considerado como Bueno";
+      description2.textContent = "El estudiante " + userName.value +" es considerado como Bueno y gano la materia";
     }else if (finalRounding >= 2.0){
-      description2.textContent = "El estudiante " + name.value + " perdio la materia pero puede recuperar";
+      description2.textContent = "El estudiante " + userName.value + " perdio la materia pero puede recuperar";
     }else if (finalRounding >= 0){
-      description2.textContent = "El estudiante " + name.value + " perdio la materia y debera repetirla";
+      description2.textContent = "El estudiante " + userName.value + " perdio la materia y debera repetirla";
     }else{
       description2.textContent = "error";
     }
